193540f6c43edc36dd36664bb07091a228113634
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2020-10-06 12:46:22 -0600

    Inital addition of header and basic styles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="app">
-    <Header />
-  </div>
+	<div id="app">
+		<Header />
+	</div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 
 export default {
-  name: 'App',
-  components: {
-    Header
-  }
+	name: 'App',
+	components: {
+		Header
+	}
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,43 @@
 <template>
-  <div></div>
+	<header class="header">
+		<h1>ORDA</h1>
+		<nav>
+			<ul>
+				<li>Our Company</li>
+				<li>Our Services</li>
+				<li>iGPS</li>
+				<li>Our Clients</li>
+				<li>Publications</li>
+				<li>Our People</li>
+				<li>Contact Us</li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+	name: 'Header'
 };
 </script>
 
-<style >
+<style lang="scss">
+.header {
+	display: flex;
+	align-items: center;
+	h1 {
+		margin: 0 20px 0 0;
+		padding: 0;
+	}
+	nav {
+		flex-grow: 1;
+		ul {
+			display: flex;
+			justify-content: space-between;
+			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+	}
+}
 </style>
