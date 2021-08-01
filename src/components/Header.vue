105472dc02c5b0599285304e0a6daa5d410c0ce1
Bryn Newell <41923284+bryn-newell@users.noreply.github.com>
2021-08-01 15:20:39 -0600

    Updating svgs and styling on services page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
 	<header class="header">
-		<router-link to="/"><h1 class="sr-only">ORDA Home</h1><ORDALogo /></router-link>
+		<router-link to="/"><h1 class="sr-only">ORDA Home</h1><ORDALogo class="orda-logo" /></router-link>
 		<nav>
 			<router-link to="/services">Our Services</router-link>
 			<router-link to="/igps">iGPS</router-link>
@@ -34,6 +34,9 @@ export default {
 	h1 {
 		margin: 0 20px 0 0;
 		padding: 0;
+	}
+	.orda-logo {
+		width: 200px;
 	}
 	nav {
 		display: flex;
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,121 +1,127 @@
 <template>
 	<section class="services">
-		<section class="service-card">
-			<p>Our clients enjoy clear insight into the hidden dynamics that exist in their organizations. They experience an ability to focus their efforts on the areas of their organizations that are preventing them from getting to the next level. The proven benefits include the ability to drastically increase retention of the best and brightest employees, which includes significantly reducing the cost of recruiting and training new employees.</p>
-			<div><nextLevel /></div>
-			<h3>Focus on the next level</h3>
-		</section>
 		<section class="service-container">
 			<div class="service-card">
+				<nextLevel />
+				<h3>Focus on the next level</h3>
+				<p>Our clients enjoy clear insight into the hidden dynamics that exist in their organizations. They experience an ability to focus their efforts on the areas of their organizations that are preventing them from getting to the next level. The proven benefits include the ability to drastically increase retention of the best and brightest employees, which includes significantly reducing the cost of recruiting and training new employees.</p>
+			</div>
+			<div class="service-card">
+				<assessAndDiagnosis />
 				<h3>Organizational Assessment & Diagnosis</h3>
 				<p>From the total organization to the smallest department, we provide an accurate, confidential assessment of strengths and needs. We assist the organization in developing specific and detailed strategies to address the immediate and future needs of the organization.</p>
-				<assessAndDiagnosis />
 			</div>
 			<div class="service-card">
+				<teamBuilding />
 				<h3>Team Building</h3>
 				<p>We have facilitated over 1,400 team building interventions. With empirical evidence that substantiates the effectiveness of this process, we offer an approach to team effectiveness that produces immediate, measurable and positive results.</p>
-				<teamBuilding />
 			</div>
 			<div class="service-card">
+				<conflictRes />
 				<h3>Conflict Resolution</h3>
 				<p>Conflict is to be expected in organizations. We have the expertise and experience to successfully resolve unhealthy conflicts, where people are unable to work effectively with one another.</p>
-				<conflictRes />
 			</div>
 			<div class="service-card">
+				<leadershipDev />
 				<h3>Leadership Development Strategies</h3>
 				<p>We offer expertise in designing and implementing leadership development strategies that are supportive of the organization's strategic plan and mission.</p>
-				<leadershipDev />
 			</div>
 			<div class="service-card">
+				<groupFacilitation />
 				<h3>Group Facilitation</h3>
 				<p>We specialize in facilitating executive, management, and Board of Director retreats. We offer unique perspectives and challenging frameworks that lead to effective and productive meetings.</p>
-				<groupFacilitation />
 			</div>
 			<div class="service-card">
+				<mergers />
 				<h3>Mergers & Acquisitions: Managing the Human Factor</h3>
 				<p>We have practical, hands-on experience in leading organizations through the difficult dynamics inherent with mergers and/or acquisitions.</p>
-				<mergers />
 			</div>
 			<div class="service-card">
+				<systemChange />
 				<h3>Large System Change and Transition Strategies</h3>
 				<p>We offer innovative and empowering participative processes that assist leadership to successfully implement large system change initiatives.</p>
-				<systemChange />
 			</div>
 			<div class="service-card">
+				<execCoaching />
 				<h3>Executive Coaching</h3>
 				<p>We understand the role of the executive. We provide unique insights and processes that enhance the executive's role in the organization.</p>
-				<execCoaching />
 			</div>
 			<div class="service-card">
+				<employeeEngagement />
 				<h3>Employee Engagement</h3>
 				<p>Most people in organizations either underestimate the power they have and/or they don't know how to use it. We offer a unique perspective on power and influence that is well received and highly effective.</p>
-				<employeeEngagement />
 			</div>
 		</section>
-		<section class="service-container">
-			<h3>Core Values</h3>
+		<section class="core-values">
 			<coreValues />
-			<p>ORDA maintains the highest level of professionalism and confidentiality. Our core values include:</p>
-			<ul>
-				<li>Integrity</li>
-				<li>Creativity</li>
-				<li>Family and Work-Life Balance</li>
-				<li>Exceed Expectations</li>
-				<li>Compassion</li>
-				<li>Excellence</li>
-			</ul>
+			<div class="content">
+				<h3>Core Values</h3>
+				<p>ORDA maintains the highest level of professionalism and confidentiality. Our core values include:</p>
+				<ul>
+					<li>Integrity</li>
+					<li>Creativity</li>
+					<li>Family and Work-Life Balance</li>
+					<li>Exceed Expectations</li>
+					<li>Compassion</li>
+					<li>Excellence</li>
+				</ul>
+			</div>
 		</section>
 		<h2>Why Are We Unique?</h2>
 		<section class="service-container">
 			<section class="service-card">
-				<h3>Experience</h3>
 				<experience />
+				<h3>Experience</h3>
 				<p>Collectively our consultants have over 155 years of proven experience working with a variety of industries.</p>
 			</section>
 			<section class="service-card">
-				<h3>Data Gathering Expertise</h3>
 				<dataGathering />
+				<h3>Data Gathering Expertise</h3>
 				<p>We work with clients to measure the variables they want to measure and collect data electronically through iGPS™. A distinguishing difference between iGPS™ and other surveys is the immediate availability of the results. Rather than requiring clients to wait four weeks to four months to get results, our clients have detailed, actionable information available within 12 seconds after closure of the assessment. iGPS™ further provides intuitive reports (including responses to open-ended questions). Clients are able to manipulate – “slice & dice” – the data on their desktop with no additional charges or waiting for adhoc reports to be developed and forwarded.</p>
 			</section>
 			<section class="service-card">
-				<h3>Bottom-line Results</h3>
 				<bottomLine />
+				<h3>Bottom-line Results</h3>
 				<p>We focus on cultural, attitudinal, financial, and customer satisfaction results and have a proven track record of success in each of these areas. Examples include helping clients increase profits through decreasing unwanted employee turnover, increasing job involvement, increasing collaboration among all levels of employees, and become “Best of Class” organizations.</p>
 			</section>
 			<section class="service-card">
-				<h3>Client Empowerment</h3>
 				<clientEmpowerment />
+				<h3>Client Empowerment</h3>
 				<p>Whereas most data collection companies limit their services to the collection and analysis of data, we help clients resolve the problems that surface in the diagnosis and gain the skills to become independent of outside assistance.</p>
 			</section>
 			<section class="service-card">
-				<h3>Long-term Results</h3>
 				<longTerm />
+				<h3>Long-term Results</h3>
 				<p>We focus on the accurate diagnosis and the permanent resolution of problems. For example, we have empirical results to support the long-term impact of team building interventions from over 1,400 teams in more than 100 organizations.</p>
 			</section>
 			<section class="service-card">
-				<h3>Emphasis on Accountability</h3>
 				<accountability />
+				<h3>Emphasis on Accountability</h3>
 				<p>Most leaders understand the need for accountability but lack the tools to hold their people accountable in ways that motivate and inspire them to achieve their potential. We provide our clients with proven mechanisms for increasing accountability, simultaneously, developing rock-solid leadership skills.</p>
 			</section>
 		</section>
-		<section class="service-container">
-			<p>Our Consultation process uses a problem-solving model that is data-based and designed to replicate the scientific method of inquiry. Simply stated, the specific steps or interventions in the action research model include:</p>
-			<ul>
-				<li>Collecting data from individuals and groups within the organization.</li>
-				<li>Reporting the data to organizational personnel.</li>
-				<li>Diagnosing the problems.</li>
-				<li>Action planning.</li>
-				<li>Reporting the diagnosis and action plan to the client system.</li>
-				<li>Implementing activities designed to facilitate change and resolve problems within the system.</li>
-			</ul>
+		<section class="model research">
 			<img :src="actionModel" alt="Research Model">
-			<h3>Action Research Model</h3>
+			<div class="content">
+				<h3>Action Research Model</h3>
+				<p>Our Consultation process uses a problem-solving model that is data-based and designed to replicate the scientific method of inquiry. Simply stated, the specific steps or interventions in the action research model include:</p>
+				<ul>
+					<li>Collecting data from individuals and groups within the organization.</li>
+					<li>Reporting the data to organizational personnel.</li>
+					<li>Diagnosing the problems.</li>
+					<li>Action planning.</li>
+					<li>Reporting the diagnosis and action plan to the client system.</li>
+					<li>Implementing activities designed to facilitate change and resolve problems within the system.</li>
+				</ul>
+			</div>
 		</section>
-		<section class="service-container">
-			<h3>Valid Information & Immediate Feedback</h3>
+		<section class="model feedback">
+			<div class="content">
+				<h3>Valid Information & Immediate Feedback</h3>
+				<p>Once these interventions have been completed, additional data are collected and fed back to the client, further problems are diagnosed, and additional interventions are planned. Action research is based on valid information and designed to provide immediate feedback to the client on the status of problems within the system. It is important to understand that e
+				ach phase of action research constitutes a set of definite interventions in the organization.</p>
+			</div>
 			<img :src="validInformation" alt="Valid Information">
-			<p>Once these interventions have been completed, additional data are collected and fed back to the client, further problems are diagnosed, and additional interventions are planned. Action research is based on valid information and designed to provide immediate feedback to the client on the status of problems within the system. It is important to understand that e
-			ach phase of action research constitutes a set of definite interventions in the organization.</p>
 		</section>
 	</section>
 </template>
@@ -172,15 +178,59 @@ export default {
 
 <style lang="scss">
 .services {
+	h2 {
+		font-size: 32px;
+	}
+	h3 {
+		font-size: 24px;
+	}
 	.service-container {
+		background-color: $green-200;
 		display: flex;
 		flex-wrap: wrap;
 		margin: 48px 0;
+		margin-right: -40px;
 	}
 	.service-card {
+		align-items: center;
 		display: flex;
 		flex-direction: column;
 		max-width: 280px;
+		margin-right: 40px;
+		text-align: center;
+		h3 {
+			margin: 16px 0;
+		}
+		svg {
+			width: 80px;
+		}
+	}
+	.core-values {
+		display: flex;
+		margin-bottom: 42px;
+		svg {
+			width: 100px;
+		}
+		.content {
+			margin-left: 48px;
+		}
+	}
+	.model {
+		align-items: center;
+		display: flex;
+		justify-content: space-between;
+		img {
+			width: 250px;
+		}
+		.content {
+			max-width: 600px;
+		}
+		&.feedback {
+			background-color: $green-200;
+			img {
+				width: 320px;
+			}
+		}
 	}
 }
 </style>
